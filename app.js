a555c45d44fde08cba7ebe4560e9213c1740c03b
Glorzo <thisaraeranga@gmail.com>
2022-02-16 20:29:17 -0600

    generate the HTML file

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    // This....
-    for(let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 
-console.log('===================================');
+const profileDataArgs = process.argv.slice(2);
 
-// Is the same as this
-// profileDataArr.forEach((profileItem) => {
-//     console.log(profileItem)
-// });
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-// much more efficient this way 
-profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+const [name, github] = profileDataArgs;
 
-// above 'printProfileData' function cleaned up using arrow function
+// console.log(profileDataArgs);
 
-printProfileData(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     // This....
+//     for(let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+
+// console.log('===================================');
+
+// // Is the same as this
+// // profileDataArr.forEach((profileItem) => {
+// //     console.log(profileItem)
+// // });
+
+// // much more efficient this way 
+// profileDataArr.forEach(profileItem => console.log(profileItem));
+// }
+
+// // above 'printProfileData' function cleaned up using arrow function
+
+// printProfileData(profileDataArgs);
+
+
+
+fs.writeFile('./index.html', generatePage(name,github),err => {
+    if(err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
