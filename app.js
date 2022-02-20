cc690e9b4c3c7f974b60886cb0a918a9ab23dd0c
Glorzo <thisaraeranga@gmail.com>
2022-02-19 22:10:51 -0600

    closing the finshed site issue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template');
+const {writeFile, copyFile} = require('./utils/generate-site');
 
 const mockData = {
     name: 'Lernantino',
@@ -180,13 +181,20 @@ Add a New Project
 };
 
 promptUser()
-    .then(portfolioData => promptProject(portfolioData))
-    .then(portfolioData => {
-        const pageHTML = generatePage(mockData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if(err) throw new Error(err);
-
-            console.log('Page created! Check out index.html in this directory to see it!');
-        });
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
